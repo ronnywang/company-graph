f197577014d6d523fbe0af98abfa067945030c80
Ronny Wang <ronnywang@gmail.com>
2014-01-12 01:38:22 +0800

    加上中心公司名稱

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -5,6 +5,12 @@ class IndexController extends Pix_Controller
     public function indexAction()
     {
         $this->view->id = intval($_GET['id']);
+        if ($this->view->id) {
+            $this->view->unit = Unit::find(intval($this->view->id));
+            if (!$this->view->unit) {
+                return $this->alert("找不到統一編號 {$this->view->id} 的公司");
+            }
+        }
     }
 
     public function jsonAction()
