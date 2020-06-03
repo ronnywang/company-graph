817e816bcca889b6f4558c268214ba47c7aefb92
Ronny Wang <ronnywang@gmail.com>
2020-06-03 17:18:09 +0800

    add config.sample.php

---
+++ webdata/config.sample.php
@@ -0,0 +1,6 @@
+<?php
+
+putenv('COMPANY_DATABASE_URL=mysql://company:company@127.0.0.1/company');
+putenv('DATABASE_URL=mysql://companygraph:companygraph@127.0.0.1/companygraph');
+putenv('GOOGLEANALYTICS_ACCOUNT=');
+
